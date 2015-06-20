b9719cdb9cac6829c3bac8303c135b5cf31b8189
JugeMC <Julian.Corash@gmail.com>
2015-06-20 09:25:19 -0700

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R file contains two functions, makeCacheMatrix and cacheSolve
 
-## Write a short comment describing this function
+## function makeCacheMatrix stores a list that contains four functions
+## set() : changes the matrix stored in the main function
+## get() : returns the matrix x stored in the main function
+## set_inverse() : stores the value of the matrix inversion
+## get_inverse() : returns the value of the matrix inversion
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) {
+        #initialize m as NULL
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        set_inverse <- function(solve) m <<- solve
+        get_inverse <- function() m
+        # the following line stores the four functions in a list:
+        list(set=set, get=get, set_inverse=set_inverse, get_inverse=get_inverse)
+        
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## The cacheSolve matrix returns a matrix that is the inversion of 'x'
+## If a matrix value exists for m, the function returns a message and the matrix inversion
+## rather than recomputing the matrix inversion
+
+cacheSolve <- function(x,...) {
+        m <- x$get_inverse()
+        # check if the matrix inversion already exists in m
+        if (!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        # get the data
+        data <- x$get()
+        # use the solve function to set the matrix inversion as m
+        m<-solve(data,...)
+        #set the new matrix inversion to m
+        x$set_inverse(m)
+        #return the matrix inversion
+        m
 }
